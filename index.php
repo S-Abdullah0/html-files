1760f627861460a3a8c1f3c889b8b259fb09e8f6
S-Abdullah0 <154699731+S-Abdullah0@users.noreply.github.com>
2023-12-24 18:32:30 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<h1 style="color:red;background: black; height: 50px;width: 150px;padding-left: 300px; ">heisenberg</h1>
+	<p>he was a actor <br> he act in movie at 2018 the movie name called Breaking bad it's a series full of make drugs and selling it</p>
+	<a href="https://youtuu.be/HhesaQXLuRY?si=WTG8dNwGZp_mlWnq"target="blank">trailer</a>
+	<audio><source src="C:\Users\kali_\Downloads\Wednesday(PaglaSongs).mp3" type="audio/mpeg">Audio not supported</audio>
+
+
+<hr>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>walter white.com</title>
+</head>
+<body>
+	<h2 style="padding-left: 250px;margin-left: 50;margin: 50px;border-width:10px ;border-style:solid;border-color: gray;">movies acted</h2>
+	<ul>breaking bad</ul><a href="https://images.app.goo.gl/kbhK35R3Re41XUQp6"target="blank">picture</a>
+	<ul>walter hartwell white</ul>
+	<ul>The upside</ul> 
+	<h3>sign up</h3>
+	<form action="next.php" method="POST">
+<div>
+		<label style="color:green;background:brown;">gmail</label>
+		<input type="text" name="name" placeholder="gmail">
+</div>
+    <footer><p>D 2063 Dinos-R-Us</p></footer>
+<div>
+		<label>password</label>
+		<input type="text,sign" name="name" placeholder="text and numbers">
+</div>
+<div>
+	    <label>gender</label>
+	    <select> name="gen"
+	    	<option value="male">male</option>
+	    	<option value="female">female</option>
+	    	
+	    </select>
+
+</div>
+<a href="">register</a>
+
+	</form>
+<hr>
+<div>
+<video controls><source src="E:\TamilVip.top_-_Mark_Antony_(2023)_Tamil_WEB_DL_1080p_AVC_(DD+5_1_192Kbps_&_AAC)_3GB_ESub_HD_720p.mp4" type="video/mp4" ></video></div>
+
+	<style type="text/css">
+		div{
+			width: 500px;
+			height: 500px;
+		}
+	</style>
+</body>
+</html>
